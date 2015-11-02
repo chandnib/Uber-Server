577da9d00d3094369c2e6f1e2ec4edf9ada57ea1
Rakshith K Hatwar <rakshithk@live.com>
2015-11-02 02:30:36 -0800

    MongoDB Integrated with RabbitMQ

--- MQDBServer.js
+++ MQDBServer.js
@@ -11,7 +11,7 @@ connection.on('ready', function() {
 			util.log("Message: "+JSON.stringify(message));
 			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
 			user.login(message, function(err,res){
-				console.log("Response : " + JSON.stringify(res));
+				console.log("verifyUser Response : " + JSON.stringify(res));
 				connection.publish(messageHeader.replyTo, res, {
 					contentType:'application/json',
 					contentEncoding:'utf-8',
@@ -28,7 +28,7 @@ connection.on('ready', function() {
 			util.log("Message: "+JSON.stringify(message));
 			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
 			user.login(message, function(err,res){
-				console.log("Response : " + JSON.stringify(res));
+				console.log("loginUser Response : " + JSON.stringify(res));
 				connection.publish(messageHeader.replyTo, res, {
 					contentType:'application/json',
 					contentEncoding:'utf-8',
@@ -45,7 +45,7 @@ connection.on('ready', function() {
 			util.log("Message: "+JSON.stringify(message));
 			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
 			user.signup(message, function(err,res){
-				console.log("Response : " + JSON.stringify(res));
+				console.log("createUser Response : " + JSON.stringify(res));
 				connection.publish(messageHeader.replyTo, res, {
 					contentType:'application/json',
 					contentEncoding:'utf-8',
@@ -63,7 +63,7 @@ connection.on('ready', function() {
 			util.log("Message: "+JSON.stringify(message));
 			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
 			user.getNewsFeed(message, function(err,res){
-				console.log("Response : " + JSON.stringify(res));
+				console.log("getNewsFeed Response : " + JSON.stringify(res));
 				connection.publish(messageHeader.replyTo, res, {
 					contentType:'application/json',
 					contentEncoding:'utf-8',
@@ -71,7 +71,194 @@ connection.on('ready', function() {
 				});
 			});
 		});
-		console.log("loginUser Queue Created!!! and listening to the Queue!");
+		console.log("getNewsFeed Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('postStatusUpdate', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.postStatusUpdate(message, function(err,res){
+				console.log("postStatusUpdate Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("postStatusUpdate Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('loadFriendList', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.loadFriendList(message, function(err,res){
+				console.log("loadFriendList Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("loadFriendList Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('sendFiendRequest', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.sendFiendRequest(message, function(err,res){
+				console.log("sendFiendRequest Response : " + JSON.stringify(res));
+					connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("sendFiendRequest Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('renderFriendListPage', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.renderFriendListPage(message, function(err,res){
+				console.log("renderFriendListPage Response : " + JSON.stringify(res));
+					connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("renderFriendListPage Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('loadMyFriendList', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.loadMyFriendList(message, function(err,res){
+				console.log("loadMyFriendList Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("loadMyFriendList Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('loadPendingFriendList', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.loadPendingFriendList(message, function(err,res){
+				console.log("loadPendingFriendList Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("loadPendingFriendList Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('rejectFriendRequest', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.rejectFriendRequest(message, function(err,res){
+				console.log("rejectFriendRequest Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("rejectFriendRequest Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('acceptFriendRequest', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.acceptFriendRequest(message, function(err,res){
+				console.log("acceptFriendRequest Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("acceptFriendRequest Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('renderUserDetailsPage', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.renderUserDetailsPage(message, function(err,res){
+				console.log("renderUserDetailsPage Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("renderUserDetailsPage Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('getLifeEvents', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.getLifeEvents(message, function(err,res){
+				console.log("getLifeEvents Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("getLifeEvents Queue Created!!! and listening to the Queue!");
+	});
+	
+	connection.queue('updateProfilePicture', function(q){
+		q.subscribe(function(message, header, deliveryInfo, messageHeader){
+			util.log(util.format( deliveryInfo.routingKey, message));
+			util.log("Message: "+JSON.stringify(message));
+			util.log("DeliveryInfo: "+JSON.stringify(deliveryInfo));
+			user.updateProfilePicture(message, function(err,res){
+				console.log("updateProfilePicture Response : " + JSON.stringify(res));
+				connection.publish(messageHeader.replyTo, res, {
+					contentType:'application/json',
+					contentEncoding:'utf-8',
+					correlationId:messageHeader.correlationId
+				});
+			});
+		});
+		console.log("updateProfilePicture Queue Created!!! and listening to the Queue!");
 	});
 	
 });
